8f0e9265db584847dfdfb0ff0bf5a479df999898
Strauzen Ethermark <strauzen@gmail.com>
2016-08-29 19:25:36 +0200

    Created a tutorial branch to fool around with the project.
    > Followed the tutorial to add text to a UI element 0_0 waaaay too basic.
    > Added a gun, the asset was meant for the master branch

---
+++ Assets/TakeOutZoo/Script/Welcome.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+namespace tutorial
+{
+    public class Welcome : MonoBehaviour
+    {
+
+        public string myMessage = "WELCOME";
+        public Text textWelcome;
+
+        // Use this for initialization
+        void Start()
+        {
+            SetInitialReference();
+            MyWelcomeMessage();
+        }
+
+        void SetInitialReference()
+        {
+            textWelcome = GameObject.Find("TextWelcome").GetComponent<Text>();
+        }
+
+        void MyWelcomeMessage()
+        {
+            if(textWelcome != null)
+            {
+                textWelcome.text = myMessage;
+            }
+            else
+            {
+                Debug.LogWarning("WelcomeText not assigned");
+            }
+        }
+    }
+}
