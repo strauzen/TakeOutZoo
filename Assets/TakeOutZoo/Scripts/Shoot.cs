f3fdd4302819e76b437620c2934a85cde2ac15b5
Strauzen Ethermark <strauzen@gmail.com>
2016-08-31 07:21:45 +0200

    Added a simple script for shooting to the player. For now it shoots debug messages.

---
+++ Assets/TakeOutZoo/Scripts/Shoot.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace tutorial
+{
+
+    public class Shoot : MonoBehaviour
+    {
+        public float fireRate = 0.3f;
+        private float nextFire;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            CheckForInput();
+        }
+
+        void CheckForInput()
+        {
+            /*
+            if (Input.GetKey(KeyCode.Mouse0) && Time.time > nextFire)
+            {
+                nextFire = Time.time + fireRate;
+                Debug.Log("Key pressed");
+            }
+            */
+
+            if (Input.GetButton("Fire1") && Time.time > nextFire)
+            {
+                nextFire = Time.time + fireRate;
+                Debug.Log("Key pressed");
+            }
+        }
+    }
+}
